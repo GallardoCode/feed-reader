af728367edb6f6c1a2f7465ab78080f8c9aaeb71
Ricardo Gallardo <rgallardouk@gmail.com>
2018-08-13 11:56:46 +0100

    Feat: Added feed entry test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -109,13 +109,23 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Inital Entries', () => { 
         /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        * function is called and completes its work, there is at least
+        * a single .entry element within the .feed container.
+        * Remember, loadFeed() is asynchronous so this test will require
+        * the use of Jasmine's beforeEach and asynchronous done() function.
+        */
+        
+        beforeEach((done) => {
+            loadFeed(0, done());
+
+        });
+
+        it('have an entry', () => {
+            expect(document.querySelector('.feed .entry-link')).toBeDefined();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
